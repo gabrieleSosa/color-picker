f668736f906802695d1fd288c305b1aed5b8b5a3
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-01-12 17:54:19 +0100

    ultima cosa mancante, colorare il bordo del list item

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,17 @@
 let colors = document.querySelectorAll(".myColor");
 let body = document.querySelector("body");
-let listItem = document.querySelectorAll("li");
+let listItem = document.querySelectorAll(".myLi");
 
 for (let i = 0; i < colors.length; i++) {
 	let c = colors[i];
 	let listIt = listItem[i];
-
+    
 	c.addEventListener("click", function (e) {
+
 		e.preventDefault();
 
 		let col = e.currentTarget;
-		console.log(col.dataset.bgcolor);
+		//console.log(col.dataset.bgcolor);
 
 		body.style.backgroundColor = col.dataset.bgcolor;
 		listIt.style.border = "3px solid black";
