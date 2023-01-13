d7c94ba49aa7fa610f1fae3eaa6ebdfb089d1691
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-01-13 11:55:32 +0100

    aggiunti colori al testo

--- static/js/index.js
+++ static/js/index.js
@@ -9,6 +9,36 @@ for (let i = 0; i < colors.length; i++) {
 		let body = document.querySelector('body');
 		body.style.backgroundColor = col.dataset.bgcolor;
 
-		
+		let mainTitle = document.querySelector('.mainTitle')
+		let subTitle = document.querySelector('.description')
+
+		switch(col.dataset.bgcolor) {
+			case '#f00':
+				mainTitle.style.color = 'white'
+				subTitle.style.color = 'white'
+				break;
+			case '#808080':
+				mainTitle.style.color = 'black'
+				subTitle.style.color = 'black'
+				break;
+			case '#000fe8':
+				mainTitle.style.color = '#832A0D'
+				subTitle.style.color = '#832A0D'
+				break;
+			case '#00a003':
+				mainTitle.style.color = 'red'
+				subTitle.style.color = 'red'
+				break;
+			case '#ff00bf':
+				mainTitle.style.color = 'yellow'
+				subTitle.style.color = 'yellow'
+				break;
+			case '#fffb00':
+				mainTitle.style.color = 'purple'
+				subTitle.style.color = 'purple'
+				break;
+			default:
+				break;
+		}
 	});
 }
