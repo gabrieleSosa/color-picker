17e1c7dbe9e62c55e8c175ee908e294970f501d3
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-01-13 11:29:26 +0100

    fine js

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,14 @@
 let colors = document.querySelectorAll(".myColor");
-let body = document.querySelector("body");
-let listItem = document.querySelectorAll(".myLi");
 
 for (let i = 0; i < colors.length; i++) {
 	let c = colors[i];
-	let listIt = listItem[i];
 
 	c.addEventListener("click", function (e) {
 		e.preventDefault();
 		let col = e.currentTarget;
-
+		let body = document.querySelector('body');
 		body.style.backgroundColor = col.dataset.bgcolor;
+
+		
 	});
 }
